57a35aa7535d5adfccfcc4fa0a8f1efce6044ad1
Jacob Tutlis <jtutlis@gmail.com>
2020-10-08 10:05:43 -0400

    cleaned files

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
-// // production only
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     });
-// }
 
 const port = process.env.PORT || 5000;
 
