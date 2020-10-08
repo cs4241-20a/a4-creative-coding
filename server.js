f27b6a85c8c72a96739bd8b70104d320b131df35
Glitch (a4-sarah-lombardi) <none>
2020-10-08 16:45:59 +0000

    🏑🚒 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
