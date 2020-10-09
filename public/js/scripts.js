7e331987478fe3a7aa273068886f708e90cca2ae
athammana <aj@virpietech.com>
2020-10-09 18:43:34 -0400

    Loaded the assets and got the basics all in now I just have to do get the game to work

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,54 @@
-// import * as THREE from '/js/three.module.js';
+import * as THREE from '/js/imports/three.module.js';
+import {OrbitControls} from '/js/imports/OrbitControls.js';
+import {GUI} from '/js/imports/dat.gui.module.js'; 
+import {GLTFLoader} from '/js/imports/GLTFLoader.js';
+
+let scene, camera, renderer, controls, container;
+
+let pawn, rook, knight, bishop, queen, king;
 
 
-let scene, camera, renderer, keyboard, clock, controls, container;
 
-let cube;
+window.onload = () => {
+	init();
+	load3dPieces();
+	animate();
+}
+
+const load3dPieces = () => {
+	let loader = new GLTFLoader();
+	loader.load('../assets/scene.gltf', (gltf) => {
+		// scene.add(pieces = gltf.scene);
+		console.log('opaisdhfa');
+		gltf.scene.traverse((obj) => {
+			if (obj.name === 'PrimaryWhiteBishop001'){
+				bishop = obj;
+			} else if (obj.name === 'PrimaryWhitePawn007'){
+				pawn = obj;
+			} else if (obj.name === 'WhiteKnight001'){
+				knight = obj;
+			} else if (obj.name === 'Rook001'){
+				rook = obj;
+			} else if (obj.name === 'WhiteQueen0'){
+				queen = obj;
+			} else if (obj.name === 'WhiteKing0'){
+				king = obj;
+			}
+		});
+
+
+	});
+}
 
 const init = () => {
+
 	//Init Scene and cameras
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xcccccc);
-	scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+	scene.background = new THREE.Color('lavender');
+	// scene.fog = new THREE.FogExp2(0xcccccc, 0.0002);
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.set(400, 200, 0);
+	camera.position.set(40, 20, 0);
 	container = document.getElementById( 'ThreeJS' );
 
 	renderer = new THREE.WebGLRenderer();
@@ -20,50 +56,47 @@ const init = () => {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
 	//Mouse Controls
-	controls = new THREE.OrbitControls( camera, renderer.domElement )
+	controls = new OrbitControls( camera, renderer.domElement )
 	controls.enableDamping = true;
 	controls.dampingFactor = .05;
 	controls.update();
-
-	keyboard = new THREEx.KeyboardState();
-	clock = new THREE.Clock();
 
 	//Bunch of random lights
 	var light = new THREE.DirectionalLight(0xffffff	);
 	light.position.set(1,1,6);
 	scene.add(light);
 
-	var light = new THREE.DirectionalLight(0x002288);
-	light.position.set(-1,-1,-1);
-	scene.add(light);
+	// var light = new THREE.DirectionalLight(0x002288);
+	// light.position.set(-1,-1,-1);
+	// scene.add(light);
 
-	var light = new THREE.AmbientLight(0x222222);
-	scene.add(light);
+	// var light = new THREE.AmbientLight(0x222222);
+	// scene.add(light);
 
 	//Pyramids
-	let geometry = new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1);
-	let material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+	// let geometry = new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1);
+	// let material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
 
-	for (var i = 0; i < 500; i ++) {
-		var mesh = new THREE.Mesh( geometry, material );
-		mesh.position.x = Math.random() * 1600 - 800;
-		mesh.position.y = 0;
-		mesh.position.z = Math.random() * 1600 - 800;
-		mesh.updateMatrix();
-		mesh.matrixAutoUpdate = false;
-		scene.add( mesh );
-	}
+	// for (var i = 0; i < 500; i ++) {
+	// 	var mesh = new THREE.Mesh( geometry, material );
+	// 	mesh.position.x = Math.random() * 1600 - 800;
+	// 	mesh.position.y = 0;
+	// 	mesh.position.z = Math.random() * 1600 - 800;
+	// 	mesh.updateMatrix();
+	// 	mesh.matrixAutoUpdate = false;
+	// 	scene.add( mesh );
+	// }
 	
 
 	//Floor
-	var floorTexture = new THREE.ImageUtils.loadTexture('../images/checkerboard.jpg');
+	var floorTexture = new THREE.ImageUtils.loadTexture('../assets/checkerboard.jpg');
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-	floorTexture.repeat.set( 10, 10 );
+	floorTexture.repeat.set(2, 2);
 	// DoubleSide: render texture on both sides of mesh
 	var floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.DoubleSide });
-	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
+	var floorGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
 	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	// floor.position.y = -0.5;
+
 	floor.rotation.x = Math.PI / 2;
 	scene.add(floor);
 
@@ -74,9 +107,14 @@ const init = () => {
 	camera.position.z = 5;
 
 
+	let gui = new GUI({name: 'chessboard'});
+	let white = gui.addFolder('White');
+	// white.add()
+	// gui.autoPlace(true);
 
-	THREEx.WindowResize(renderer, camera);
-	THREEx.FullScreen.bindKey({ charCode : 'f'.charCodeAt(0) });
+
+	// THREEx.WindowResize(renderer, camera);
+	// THREEx.FullScreen.bindKey({ charCode : 'f'.charCodeAt(0) });
 
 	container.appendChild(renderer.domElement);
 
@@ -101,6 +139,5 @@ const animate = () => {
 	renderer.render(scene, camera);
 }
 
-init();
-
-animate();
+//Add stats box
+(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
